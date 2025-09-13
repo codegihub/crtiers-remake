c5494922979473b845f9ad0067584dea92c12d34
codegihub <codehubemail@gmail.com>
2025-09-13 16:29:50 +0300

    dsada

--- app/more/page.tsx
+++ app/more/page.tsx
@@ -11,10 +11,10 @@ export default function More() {
             <h1 className="gradient-text">CrTiers</h1>
           </div>
           <div className={styles.navLinks}>
-            <a href="./" className={styles.navLink}>Home</a>
-            <a href="./leaderboards" className={styles.navLink}>Leaderboards</a>
-            <a href="./server" className={styles.navLink}>Server</a>
-            <a href="./more" className={styles.navLink}>More</a>
+            <a href="../.." className={styles.navLink}>Home</a>
+            <a href="../../leaderboards" className={styles.navLink}>Leaderboards</a>
+            <a href="../../server" className={styles.navLink}>Server</a>
+            <a href="../../more" className={styles.navLink}>More</a>
           </div>
         </nav>
       </header>
