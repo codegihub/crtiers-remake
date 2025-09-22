febdb70a82a0e33f5d8d6a538d2a20a4d9d7c6f7
codegihub <codehubemail@gmail.com>
2025-09-22 16:29:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7875356554712566" 
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <Analytics />
